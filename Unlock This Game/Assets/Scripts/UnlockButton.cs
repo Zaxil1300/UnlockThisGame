ee162ba64799b4cc36a664455204f8f9dc6ff1c7
Zaxil30 <aguzmanjr94@gmail.com>
2024-03-30 15:26:51 -0400

    Basic Button Clicking Setup

---
+++ Unlock This Game/Assets/Scripts/UnlockButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UnlockButton : MonoBehaviour
+{
+    public int m_ClickCount;
+    public TextMeshProUGUI m_CountText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseCount () 
+    {
+        m_ClickCount++;
+        m_CountText.text = m_ClickCount.ToString();
+    }
+    public void BuyFeature()
+    {
+
+    }
+}
